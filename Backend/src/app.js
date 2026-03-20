ded39044be7a19fecffef265dd26013026378d6d
Ordinary-Boy20 <embeddedsystem481@gmail.com>
2026-03-20 15:25:02 +0530

    Add / for checking

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -20,6 +20,8 @@ const interviewRouter = require("./routes/interview.routes")
 app.use("/api/auth", authRouter)
 app.use("/api/interview", interviewRouter)
 
-
+app.get("/", (req, res) => {
+  res.send("AI Interview Backend is running 🚀");
+});
 
 module.exports = app
