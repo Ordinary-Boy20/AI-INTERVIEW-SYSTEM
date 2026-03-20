86749f31d921b0f0e97f6c0a4ed66c764562e96f
Ordinary-Boy20 <embeddedsystem481@gmail.com>
2026-03-20 15:11:11 +0530

    changes in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,6 +5,6 @@ const connectToDB = require("./src/config/database")
 connectToDB()
 
 
-app.listen(Process.env.PORT || 5000, () => {
-    console.log("Server is running on port " + (Process.env.PORT || 5000))
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server is running on port " + (process.env.PORT || 5000))
 })
